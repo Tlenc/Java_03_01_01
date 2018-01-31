d89114c5d403de99ba536183a872ad9f7f2e80d7
Tlenc <tlenciauskas@gmail.com>
2018-01-31 11:17:39 +0200

    Code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Įveskite sveiką skaičių");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        if (a % 7 == 0) {
+            System.out.println(a + " Dalinasi iš 7");
+        }
+        if (a % 5 == 0) {
+            System.out.println(a + " Dalinasi iš 5");
+        }
+        if (a % 3 == 0) {
+            System.out.println(a + " Dalinasi iš 3");
+        } else {
+            System.out.println(a + " Nesidalina");
+        }
     }
 }
